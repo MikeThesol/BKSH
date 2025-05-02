20727ccc8c6f8ca0a2e809fecb79b075fd22132f
MikeThesol <mike151186@gmail.com>
2025-05-02 16:57:11 +0300

    update main file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/MikeThesol/BKSH/internal/config"
+import (
+	"github.com/MikeThesol/BKSH/internal/config"
+	"github.com/MikeThesol/BKSH/internal/repository"
+	"github.com/MikeThesol/BKSH/internal/service"
+)
 
 func main() {
+	cfg := config.MustLoad()
 	log := config.InitLogger("local")
 	log.Info("server start")
+
+	db, err := repository.NewPostgresDB(cfg)
+	if err != nil {
+		log.Error("DB not create", err.Error())
+		return
+	}
+	repository := repository.NewRepository(db)
+	service := service.NewService(repository)
+	_ = service
 }
